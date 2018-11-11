d1ab7b1cc29ceea4a2f5fb504c37cd72dd7cf694
Pat <27511950+ThePat02@users.noreply.github.com>
2018-11-11 16:48:56 +0100

    Added firebase

---
+++ scripts/data-firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+
+//https://firebase.google.com/docs/database/web/read-and-write?authuser=0
+var config = {
+    apiKey: "AIzaSyDLAKAl0qALxVk8DWn7UHGAcqOetk0H5mY",
+    authDomain: "game-highscore-project.firebaseapp.com",
+    databaseURL: "https://game-highscore-project.firebaseio.com",
+    projectId: "game-highscore-project",
+    storageBucket: "",
+    messagingSenderId: "636497383005"
--- scripts/master.js
+++ scripts/master.js
@@ -19,6 +19,28 @@ function loadScripts(arg)
   s.src = "scripts/savingsystem.js";
   $("head").append(s);
 
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "https://www.gstatic.com/firebasejs/5.5.8/firebase-app.js";
+  $("head").append(s)
+
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "https://www.gstatic.com/firebasejs/5.5.8/firebase.js";
+  $("head").append(s);
+
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "https://www.gstatic.com/firebasejs/5.5.8/firebase-database.js";
+  $("head").append(s);
+
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "scripts/data-firebase.js";
+  $("head").append(s);
+
+
+
   if (arg == true) startUp();
 }
 
