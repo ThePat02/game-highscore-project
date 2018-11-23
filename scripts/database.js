4e64243eec577f9ec69557e86766e5ad89a93cde
Pat <27511950+ThePat02@users.noreply.github.com>
2018-11-23 06:23:43 +0100

    Added those two

---
+++ scripts/database.js
@@ -0,0 +1,10 @@
+function getData()
+{
+  var ref = firebase.database().ref();
+
+ref.on("value", function(snapshot) {
+   console.log(snapshot.val());
+}, function (error) {
+   console.log("Error: " + error.code);
+});
+}
