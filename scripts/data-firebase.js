13c6546674716ff9029376a97ee52d4dffd36d49
Pat <27511950+ThePat02@users.noreply.github.com>
2018-11-23 06:23:30 +0100

    Fixed }

--- scripts/data-firebase.js
+++ scripts/data-firebase.js
@@ -2,9 +2,14 @@
 
 //https://firebase.google.com/docs/database/web/read-and-write?authuser=0
 var config = {
-    apiKey: "AIzaSyDLAKAl0qALxVk8DWn7UHGAcqOetk0H5mY",
-    authDomain: "game-highscore-project.firebaseapp.com",
-    databaseURL: "https://game-highscore-project.firebaseio.com",
-    projectId: "game-highscore-project",
-    storageBucket: "",
-    messagingSenderId: "636497383005"
+  apiKey: "AIzaSyDLAKAl0qALxVk8DWn7UHGAcqOetk0H5mY",
+  authDomain: "game-highscore-project.firebaseapp.com",
+  databaseURL: "https://game-highscore-project.firebaseio.com",
+  projectId: "game-highscore-project",
+  storageBucket: "",
+  messagingSenderId: "636497383005"
+};
+
+if (!firebase.apps.length) {
+   firebase.initializeApp(config);
+}
